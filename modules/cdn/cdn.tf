de7d21c284f9e742cbfa0fad65405f259e437884
Nikhil <nikd9917@gmail.com>
2022-12-22 12:00:50 +0530

    updated CDN and R53

--- modules/cdn/cdn.tf
+++ modules/cdn/cdn.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_distribution" "ALB_distribution" {
   origin {
-    domain_name              = "pub-ealb-nginx-2038487345.ap-south-1.elb.amazonaws.com"//var.domain_name  
+    domain_name              = var.domain_name //"pub-ealb-nginx-2038487345.ap-south-1.elb.amazonaws.com"// 
     origin_id                = var.origin_id
 
     custom_origin_config {
@@ -14,9 +14,6 @@ resource "aws_cloudfront_distribution" "ALB_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "CloudFront distribution with ALB origin"
-  # default_root_object = "index.html"
-
-  # aliases = ["mysite.example.com", "yoursite.example.com"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -36,52 +33,6 @@ resource "aws_cloudfront_distribution" "ALB_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
-
-  # Cache behavior with precedence 0
-  # ordered_cache_behavior {
-  #   path_pattern     = "/content/immutable/*"
-  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-  #   cached_methods   = ["GET", "HEAD", "OPTIONS"]
-  #   # target_origin_id = local.s3_origin_id
-
-  #   forwarded_values {
-  #     query_string = false
-  #     headers      = ["Origin"]
-
-  #     cookies {
-  #       forward = "none"
-  #     }
-  #   }
-
-  #   min_ttl                = 0
-  #   default_ttl            = 86400
-  #   max_ttl                = 31536000
-  #   compress               = true
-  #   viewer_protocol_policy = "redirect-to-https"
-  # }
-
-  # Cache behavior with precedence 1
-  # ordered_cache_behavior {
-  #   path_pattern     = "/content/*"
-  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-  #   cached_methods   = ["GET", "HEAD"]
-  #   # target_origin_id = local.s3_origin_id
-
-  #   forwarded_values {
-  #     query_string = false
-
-  #     cookies {
-  #       forward = "none"
-  #     }
-  #   }
-
-  #   min_ttl                = 0
-  #   default_ttl            = 3600
-  #   max_ttl                = 86400
-  #   compress               = true
-  #   viewer_protocol_policy = "redirect-to-https"
-  # }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
--- modules/cdn/output.tf
+++ modules/cdn/output.tf
@@ -1,3 +1,7 @@
+output "cdn-hosted_zone_domain" {
+  value = aws_cloudfront_distribution.ALB_distribution.domain_name
+}
+
 output "cdn-hosted_zone_id" {
   value = aws_cloudfront_distribution.ALB_distribution.hosted_zone_id
 }
